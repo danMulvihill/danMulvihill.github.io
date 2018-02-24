96845072a2b3827a90b8be1ff6ab1d0990484cbf
Dan Mulvihill <dmulvihill3@gmail.com>
2018-02-23 23:13:36 -0500

    some changes; font

--- js/main.js
+++ js/main.js
@@ -3,31 +3,31 @@ document.getElementById("backImg").addEventListener("click", addImg );
 document.getElementById("show").addEventListener("click", showControl );
 document.getElementById("hide").addEventListener("click", hideControl );
 
-var $on;
+var imageOn;
 function addImg() {
 //toggles through different background images depending on user input  
-  if(!$on){
+  if(!imageOn){
     //document.getElementById("whatsOn").innerText = $on;
-    $on = 1;
+    imageOn = 1;
     
     document.body.style.backgroundImage = "url(img/1.jpg)";
     document.getElementById("backImg").innerText = "Change Background Image";
     
   }else{
    //document.getElementById("whatsOn").innerText = $on;
-   $on += 1;
-   if($on==7) document.getElementById("backImg").innerText = "Remove Background Image";
-   if($on==8) {
-   	$on=null;
+   imageOn += 1;
+   if(imageOn==7) document.getElementById("backImg").innerText = "Remove Background Image";
+   if(imageOn==8) {
+   	imageOn=null;
     	document.getElementById("backImg").innerText = "Add Background Image";
     	}
-   document.body.style.backgroundImage = "url(img/"+$on+".jpg)";
+   document.body.style.backgroundImage = "url(img/"+imageOn+".jpg)";
    }
 }
 
-function upOp($opLevel){
+function upOp(opLevel){
 //changes the opacity of the container with text
-    document.getElementById("container").style.opacity = $opLevel;
+    document.getElementById("container").style.opacity = opLevel;
 }
 function hideControl(){
     document.getElementById("control").style.display = "none";
@@ -37,3 +37,24 @@ function hideControl(){
     document.getElementById("control").style.display = "block";
     document.getElementById("ctrl").style.display = "none";
  }
+
+
+
+//By default (on page load)
+//if a firstName cookie is set, update the DOM to display the val of the cookie
+// window.addEventListener("load", function(){ 
+//   if (Cookies.get('fname')){
+//     console.log("found it");
+//     document.getElementById("greeting").innerHTML = "Welcome Back, "+ Cookies.get('fname');   
+//   }
+// });
+
+//gets and sets a cookie:
+
+//Cookies.set("fname","Dan");
+//document.getElementById("button").addEventListener("click", function(){
+//   var fname = document.getElementById("fname").value;
+//   Cookies.set("fname", fname);
+//   document.getElementById("greeting").style.display = "none";
+//   document.getElementById("cookieform").innerHTML = "Enjoy your cookie, " +fname; 
+//});
