14b3abe91dd90c9cefa5236e4761142b7fca665f
Dan Mulvihill <dmulvihill3@gmail.com>
2018-03-05 13:09:58 -0500

    made some fixes to js cookies

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 
-//jQuery animate:
+
 
 $(document).ready(function(){
-   var cookieSet = !Cookies.get('imageCookie') && !Cookies.get('colorChoice');
-   console.log(cookieSet);
-   if(cookieSet){
-      
+  //cookies store the user-chosen settings from previous visits
+   var noCookiesFound = !Cookies.get('userSet.image') && !Cookies.get('userSet.color');
+   
+   if(noCookiesFound){
+      //animation show if no cookies on browser
       $("#col1").animate({
           left: '215px'
       },250);
@@ -30,32 +31,33 @@ $(document).ready(function(){
     $("#col2").css("left", "190px");
     $("#col3").css("left","165px");
     $("#col4").css("left", "140px");
-  }
 
-    if (Number(Cookies.get('imageCookie'))>0){
-      console.log("imageCookie: ", Cookies.get("imageCookie"));
-      imageOn = Number(Cookies.get("imageCookie"));
+    //uses the cookie data to determine which settings to set
+    if (Number(Cookies.get('userSet.image'))>0){
+      imageOn = Number(Cookies.get("userSet.image"));
       document.body.style.backgroundImage = "url(img/"+imageOn+".jpg)";
       if(imageOn) document.getElementById("backImg").innerText = "Change Background Image";
-      opLevel = Cookies.get("opLevel");
+      opLevel = Cookies.get("userSet.opLevel");
       document.getElementById("container").style.opacity = opLevel; 
       document.querySelector("#color-panel").style.display = "none";
       document.getElementById("opControl").style.display = "inline";
+      //console.log("userSet.image: ", Cookies.get("userSet.image"));
       
-    }else if (Cookies.get('colorChoice')){
-      console.log("colorChoie cookie: ", Cookies.get("colorChoice"));
+    }else if (Cookies.get('userSet.color')){
       imageOn = 0;
-      colorChoice = Cookies.get("colorChoice");
+      colorChoice = Cookies.get("userSet.color");
       document.body.style.backgroundColor = colorChoice;
-      
+      //console.log("colorChoie cookie: ", Cookies.get("userSet.color"));
 
     }else{
       imageOn = 0;
-      console.log("no cookie", imageOn)
+      //console.log("no cookie", imageOn)
     }
+  }
 });//end document.ready
 
 
+//click event listeners to get data from user 
 document.getElementById("backImg").addEventListener("click", addImg );
 document.getElementById("show").addEventListener("click", showControl );
 document.getElementById("hide").addEventListener("click", hideControl );
@@ -64,57 +66,58 @@ document.getElementById("hide").addEventListener("click", hideControl );
 function addImg() {
 //toggles through different background images depending on user input  
   if(!imageOn){
-    console.log("first if", imageOn);
+    //console.log("first if", imageOn);
     imageOn = 1;
     
     document.body.style.backgroundImage = "url(img/1.jpg)";
     document.getElementById("backImg").innerText = "Change Background Image";
     document.querySelector("#color-panel").style.display = "none";
     document.getElementById("opControl").style.display = "inline";
-    Cookies.set("imageCookie", imageOn);
+    Cookies.set("userSet.image", imageOn);
   }else{
-   console.log("else", imageOn);
+   //console.log("else", imageOn);
    imageOn += 1;
-   Cookies.set("imageCookie",imageOn);
+   Cookies.set("userSet.image",imageOn);
    if(imageOn==4) {
       document.getElementById("backImg").innerText = "Remove Background Image";
        
    }
    if(imageOn==5) {
-        imageOn=null;
-        Cookies.set("imageCookie",imageOn);
+        imageOn=0;
+        Cookies.set("userSet.image",imageOn);
         document.getElementById("backImg").innerText = "Add Background Image";
         document.querySelector("#color-panel").style.display = "inline";
         document.getElementById("opControl").style.display = "none";
     	}
-   document.body.style.backgroundImage = "url(img/"+imageOn+".jpg)";
+   if(imageOn) document.body.style.backgroundImage = "url(img/"+imageOn+".jpg)";
+   else document.body.style.backgroundImage = null;
    }
-}
-
-function changeCol(colorChoice){
-  document.body.style.backgroundColor = colorChoice;
-  Cookies.set("colorChoice", colorChoice);
-}
-
-function upOp(opLevel){
-//changes the opacity of the container with text
-    document.getElementById("container").style.opacity = opLevel; 
-    Cookies.set("opLevel", opLevel);
 }
 
 //toggles the hide/show button
 function hideControl(){
-    document.getElementById("toggle-hide").style.display = "none";
-    document.getElementById("hide").style.display = "none";
-    document.getElementById("show").style.display = "inline";
+  document.getElementById("toggle-hide").style.display = "none";
+  document.getElementById("hide").style.display = "none";
+  document.getElementById("show").style.display = "inline";
+}
+function showControl(){
+  document.getElementById("toggle-hide").style.display = "inline";
+  document.getElementById("hide").style.display = "inline";
+  document.getElementById("show").style.display = "none";
+}
 
- }
- function showControl(){
-    document.getElementById("toggle-hide").style.display = "inline";
-    document.getElementById("hide").style.display = "inline";
-    document.getElementById("show").style.display = "none";
- }
 
+//next two functions changes bg color and opacity of image
+//they are triggered by event attrs for now. 
+function changeCol(colorChoice){
+  document.body.style.backgroundColor = colorChoice;
+  Cookies.set("userSet.color", colorChoice);
+}
+
+function upOp(opLevel){
+    document.getElementById("container").style.opacity = opLevel; 
+    Cookies.set("userSet.opLevel", opLevel);
+}
 
 
 
